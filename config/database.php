e439361528e8b0cc6b6262040abf760d59312001
Skill <Skill@DESKTOP-VMP37H7>
2021-05-29 22:54:43 +0200

    mysql -> pqsql /config/database.php

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'), # mysql pgsql
+    'default' => env('DB_CONNECTION', 'pgsql'), # mysql pgsql
 
     /*
     |--------------------------------------------------------------------------
